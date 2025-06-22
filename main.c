aa620698c3a527842520492ee25e714b2606c8b2
kimchiheon <clgjs1025@naver.com>
2025-06-22 21:06:20 +0900

    main 3차 수정

--- main.c
+++ main.c
@@ -5,12 +5,18 @@
 
 #include "quiz_game.h"
 #include "file_manager.h"
-#include "quiz_engine.h"
-#include "ui_utils.h"
-#include "admin.h"  
+#include "admin.h"
+#include "quiz_engine.h"  // 추가
 
 #define MAX_QUESTIONS 100
 #define MAX_NAME_LEN 50
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 void getLine(char* buffer, int size) {
     fgets(buffer, size, stdin);
@@ -18,16 +24,25 @@ void getLine(char* buffer, int size) {
 }
 
 int main() {
+    #ifdef _WIN32
+#include <windows.h>
+#endif
+
+    #ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8); // 출력 인코딩을 UTF-8로 설정
+    SetConsoleCP(CP_UTF8);       // 입력 인코딩도 UTF-8로 설정
+    #endif
     int mode;
     printf("=== 넌센스 퀴즈 게임 ===\n\n");
-    printf("1. 퀴즈 풀기\n");
+    printf("1. 퀴즈 풀기 (텍스트 기반)\n");
     printf("2. 관리자 모드\n");
+    printf("3. 제한시간 객관식 퀴즈\n");
     printf("0. 종료\n");
     printf("모드를 선택하세요: ");
     scanf("%d", &mode);
-    getchar();  
+    getchar();
 
-    if (mode == 3) {
+    if (mode == 0) {
         printf("프로그램을 종료합니다.\n");
         return 0;
     }
@@ -113,6 +128,45 @@ int main() {
             displaySavedResults(10);
         }
     }
+    else if (mode == 3) {
+        // 타이머 기반 객관식 퀴즈 모드
+        Quiz* head = NULL;
+        Quiz* current = NULL;
+
+        // 하드코딩 예제 문제들
+        const char* questions[] = {
+            "1. C 언어에서 'int'는 어떤 자료형인가요?\nA) 정수 B) 실수 C) 문자 D) 포인터",
+            "2. 대한민국의 수도는 어디인가요?\nA) 부산 B) 대전 C) 서울 D) 인천",
+            "3. 바다에서 가장 짠 바다는?\nA) 발트해 B) 홍해 C) 지중해 D) 동해"
+        };
+        const char answers[] = { 'A', 'C', 'B' };
+
+        for (int i = 0; i < 3; i++) {
+            Quiz* newQuiz = (Quiz*)malloc(sizeof(Quiz));
+            strcpy(newQuiz->question, questions[i]);
+            newQuiz->answer = answers[i];
+            newQuiz->score = 0;
+            newQuiz->next = NULL;
+
+            if (head == NULL) {
+                head = newQuiz;
+                current = newQuiz;
+            } else {
+                current->next = newQuiz;
+                current = newQuiz;
+            }
+        }
+
+        shuffleLinkedList(&head, 3);
+        runQuiz(head);
+
+        // 메모리 해제
+        while (head != NULL) {
+            Quiz* temp = head;
+            head = head->next;
+            free(temp);
+        }
+    }
     else {
         printf("잘못된 선택입니다.\n");
         return 1;
@@ -120,4 +174,3 @@ int main() {
 
     return 0;
 }
-
